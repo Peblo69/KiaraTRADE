5b028c65b47abef47e523e091b2c3e88b92718d9
Johnny Depp <38878337-kacinka74@users.noreply.replit.com>
2025-02-04 10:38:17 +0000

    Restored to '66d3aaa8ec0b235befa477f92e7699ded3650039'
    Replit-Restored-To:66d3aaa8ec0b235befa477f92e7699ded3650039

--- client/src/pages/pumpfun-vision.tsx
+++ client/src/pages/pumpfun-vision.tsx
@@ -116,20 +116,21 @@ const PumpFunVision: FC = () => {
                     </div>
                   </div>
 
-                  {/* Right Column - Trading Form & Wallet Tracker */}
-                  <div className="col-span-3 h-full flex flex-col gap-4">
-                    <div className="bg-[#0D0B1F] rounded-lg border border-purple-900/30 p-4 h-[50%]">
+                  {/* Right Column - Trading Form */}
+                  <div className="col-span-3 h-full">
+                    <div className="bg-[#0D0B1F] rounded-lg border border-purple-900/30 p-4 h-full">
                       <TradingForm tokenAddress={selectedToken} />
-                    </div>
-                    <div className="bg-[#0D0B1F] rounded-lg border border-purple-900/30 p-4 h-[50%]">
-                      <WalletTracker />
                     </div>
                   </div>
                 </div>
-                {/* Bottom Section - Holder Analytics */}
-                <div style={{ marginTop: "70px" }}>
+                
+                {/* Bottom Section - Holder Analytics & Wallet Tracker */}
+                <div className="grid grid-cols-2 gap-4 mt-4">
                   <div className="bg-[#0D0B1F] rounded-lg border border-purple-900/30 p-4">
                     <HolderAnalytics tokenAddress={selectedToken} />
+                  </div>
+                  <div className="bg-[#0D0B1F] rounded-lg border border-purple-900/30 p-4">
+                    <WalletTracker />
                   </div>
                 </div>
               </div>
