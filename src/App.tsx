29dbef8b4faf955dc044dd6e86b9e667331d83da
kacinka74 <38878337-kacinka74@users.noreply.replit.com>
2025-02-07 01:00:17 +0000

    Agent query: What page do you currently see when accessing the app? Do you see a wallet page or something else?
    Refactor: Simplify App component, removing unnecessary routes and react-query provider.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/63239fcc-e510-4796-b18e-5db94e48d82c/3ecf5b22-8032-4e53-a763-4dd0b8db124a.jpg

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,23 @@
 import React from 'react';
-import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "./lib/queryClient";
-import { Route } from 'wouter';
-
-// Feature Components
-import { KiaraVideo } from './components/features/KiaraVideo';
 import { SpaceBackground } from './components/features/SpaceBackground';
+import { KiaraVideo } from './components/features/KiaraVideo';
 import { AiChat } from './components/features/AiChat';
-
-// Layout Components
 import { TopBar } from './components/layout/TopBar';
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <div className="min-h-screen bg-[#0B0B1E] grid-bg">
-        <SpaceBackground />
-        <TopBar />
+    <div className="min-h-screen bg-[#0B0B1E] grid-bg">
+      <SpaceBackground />
+      <TopBar />
 
-        <main>
-          <Route path="/">
-            <div className="container mx-auto px-4 py-6">
-              <KiaraVideo />
-            </div>
-          </Route>
-          {/*The following routes are removed based on the edited code, but may need to be re-added if the intention changes.*/}
-          {/*<Route path="/token/:tokenAddress" component={TokenDetails} />
-          <Route path="/trading">
-            {() => (
-              <div className="container mx-auto px-4 py-6">
-                <div className="grid grid-cols-12 gap-4">
-                  <div className="col-span-2 space-y-4">
-                    <MarketStats />
-                    <SocialMetrics />
-                  </div>
-                  <div className="col-span-7 space-y-4">
-                    <TradingChart />
-                    <TradeHistory />
-                  </div>
-                  <div className="col-span-3 space-y-4">
-                    <TradingForm />
-                    <HolderAnalytics />
-                  </div>
-                </div>
-              </div>
-            )}
-          </Route>*/}
-        </main>
+      <main>
+        <div className="container mx-auto px-4 py-6">
+          <KiaraVideo />
+        </div>
+      </main>
 
-        <AiChat />
-        {/* <Toaster /> This is removed as per the edited code. */}
-      </div>
-    </QueryClientProvider>
+      <AiChat />
+    </div>
   );
 }
 
